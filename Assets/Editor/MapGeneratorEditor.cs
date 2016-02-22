8fc33898b85d6885142c6e6f5cae4528f9620ffd
cor <cor@pruijs.nl>
2016-02-22 23:38:32 +0100

    Add auto updating map generator

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor (typeof (MapGenerator))]
+public class MapGeneratorEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        MapGenerator mapGenerator = (MapGenerator) target;
+        
+        if (DrawDefaultInspector()) {
+            if(mapGenerator.shouldAutoUpdate) {
+                mapGenerator.GenerateMap();
+            }
+        }
+        
+        if(GUILayout.Button("Generate")) {
+            mapGenerator.GenerateMap();
+        }
+    }
+}
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour {
+
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+ 
+    public bool shouldAutoUpdate;
+    
+    public void GenerateMap() {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        
+        MapDisplay display = FindObjectOfType<MapDisplay> ();
+        display.DrawNoiseMap(noiseMap);
+    }
+}
