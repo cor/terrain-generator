a23f9f00a2e4645877063154d1438c2b7d3a1e53
cor <cor@pruijs.nl>
2016-02-24 19:40:13 +0100

    Add Octaves, Persistance and Lacunarity

--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -3,23 +3,47 @@ using System.Collections;
 
 public static class Noise {
     
-    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale) {
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, int octaves, float persistance, float lacunarity) {
        float[,] noiseMap = new float[mapWidth, mapHeight];
        
        if (scale <= 0) {
            scale = 0.0001f;
        }
        
+       float maxNoiseHeight = float.MinValue;
+       float minNoiseHeight = float.MaxValue;
+       
        for (int y = 0; y < mapHeight; y++) {
            for (int x = 0; x < mapWidth; x++) {
-               float sampleX = x / scale;
-               float sampleY = y / scale;
-               
-               float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
-               noiseMap[x,y] = perlinValue;
+              
+              float amplitude = 1;
+              float frequency = 1;
+              float noiseHeight = 0;
+              
+              for (int i = 0; i < octaves; i++) {
+                   float sampleX = x / scale * frequency;
+                   float sampleY = y / scale * frequency;
+              
+                   float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                   noiseHeight += perlinValue * amplitude;
+                   
+                   amplitude *= persistance;
+                   frequency *= lacunarity;
+              }
+              if (noiseHeight > maxNoiseHeight) {
+                  maxNoiseHeight = noiseHeight;
+              } else if (noiseHeight < minNoiseHeight) {
+                  minNoiseHeight = noiseHeight;
+              }
+              noiseMap[x,y] = noiseHeight;
            }
        }
        
-       return noiseMap;
+        for (int y = 0; y < mapHeight; y++) {
+            for (int x = 0; x < mapWidth; x++) {
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x,y]);
+            }     
+        }
+        return noiseMap;
     }
 }
