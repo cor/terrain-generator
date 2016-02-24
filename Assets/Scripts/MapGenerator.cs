3311dc1fd1175925af86734b3a86dfcb9508aea8
cor <cor@pruijs.nl>
2016-02-24 19:58:04 +0100

    Add seed, offset and improve editor

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -8,15 +8,39 @@ public class MapGenerator : MonoBehaviour {
     public float noiseScale;
  
     public int octaves;
+    [Range(0,1)]
     public float persistance;
     public float lacunarity;
+ 
+    public int seed;
+    public Vector2 offset;
  
     public bool shouldAutoUpdate;
     
     public void GenerateMap() {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale, octaves, persistance, lacunarity);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
         
         MapDisplay display = FindObjectOfType<MapDisplay> ();
         display.DrawNoiseMap(noiseMap);
+    }
+    
+    void OnValidate() {
+        
+        // Assert correct values
+        if (mapWidth < 1) {
+            mapWidth = 1;
+        }
+        
+        if (mapHeight < 1) {
+            mapHeight = 1;
+        }
+        
+        if (lacunarity < 1) {
+            lacunarity = 1;
+        }
+        
+        if (octaves < 0) {
+            octaves = 0;
+        }
     }
 }
--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -3,8 +3,28 @@ using System.Collections;
 
 public static class Noise {
     
-    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, int octaves, float persistance, float lacunarity) {
+    public static float[,] GenerateNoiseMap(
+        int mapWidth, 
+        int mapHeight, 
+        int seed, 
+        float scale, 
+        int octaves, 
+        float persistance, 
+        float lacunarity,
+        Vector2 offset) {
+            
+       // Create a new 2D float array based on map width and height
        float[,] noiseMap = new float[mapWidth, mapHeight];
+       
+       System.Random prng = new System.Random(seed);
+       Vector2[] octaveOffsets = new Vector2[octaves];
+       
+       // Generate random offsets
+       for (int i = 0; i < octaves; i++) {
+           float offsetX = prng.Next(-100000, 100000) + offset.x;
+           float offsetY = prng.Next(-100000, 100000) + offset.y;
+           octaveOffsets[i] = new Vector2(offsetX, offsetY);
+       }
        
        if (scale <= 0) {
            scale = 0.0001f;
@@ -12,6 +32,10 @@ public static class Noise {
        
        float maxNoiseHeight = float.MinValue;
        float minNoiseHeight = float.MaxValue;
+       
+       // Calculate the center of the map for zooming on the center
+       float halfWidth = mapWidth / 2f;
+       float halfHeight = mapHeight / 2f; 
        
        for (int y = 0; y < mapHeight; y++) {
            for (int x = 0; x < mapWidth; x++) {
@@ -21,8 +45,8 @@ public static class Noise {
               float noiseHeight = 0;
               
               for (int i = 0; i < octaves; i++) {
-                   float sampleX = x / scale * frequency;
-                   float sampleY = y / scale * frequency;
+                   float sampleX = (x - halfWidth) / scale * frequency + octaveOffsets[i].x;
+                   float sampleY = (y - halfHeight) / scale * frequency + octaveOffsets[i].y;
               
                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
                    noiseHeight += perlinValue * amplitude;
